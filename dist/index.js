876a497bc197decb36f23f44c0f85f0e31bc2128
Grzegorz Dlugokecki <grzesiek.dlugokecki@gmail.com>
2023-11-21 11:00:06 +0100

    Initial

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+import fastify from "fastify";
+const server = fastify();
+import postgres from "@fastify/postgres";
+server.register(postgres, {
+    connectionString: "postgresql://postgres:postgres@host.docker.internal:5432/postgres",
+});
+server.get("/ping", async (request, reply) => {
+    const client = await server.pg.connect();
+    return "pong\n";
+});
+server.listen({ port: 8080 }, (err, address) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+});
