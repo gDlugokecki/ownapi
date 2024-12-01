d87c1e6df2b0a7694aa094bee33f687266238e58
Grzegorz Dlugokecki <grzesiek.dlugokecki@gmail.com>
2024-12-01 13:19:22 +0100

    WIP map api

--- src/index.ts
+++ src/index.ts
@@ -120,7 +120,7 @@ Note: Only return the JSON coordinates, do not add any formatting like \`\`\`jso
   }
 );
 
-server.listen({ port: 8080, host: "0.0.0.0" }, (err, address) => {
+server.listen({ port: 50419, host: "0.0.0.0" }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
