724a8d1983ffb99cb8b5eba82ce2ed203e32b9e1
Grzegorz Dlugokecki <grzesiek.dlugokecki@gmail.com>
2024-12-01 11:49:58 +0100

    Changed port

--- src/index.ts
+++ src/index.ts
@@ -73,7 +73,7 @@ server.post(
   }
 );
 
-server.listen({ port: 8080, host: "0.0.0.0" }, (err, address) => {
+server.listen({ port: 50419, host: "0.0.0.0" }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
